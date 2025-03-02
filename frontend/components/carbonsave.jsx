6192a3e6d2b7e7af2e112a7edd684f2448650bcb
Deep <deeppatel223204@gmail.com>
2025-03-02 10:29:26 +0530

    carbon save->carbon emission

--- frontend/components/carbonsave.jsx
+++ frontend/components/carbonsave.jsx
@@ -7,7 +7,9 @@ const CarbonSave = ({ value }) => {
     <View className="flex-row items-center px-2 py-1 rounded-lg bg-surface self-start border border-gray-800">
       <FontAwesome5 name="leaf" size={16} color="#00b890" className="mr-2" />
       <View>
-        <Text className="text-xs font-medium text-primary">Carbon Save</Text>
+        <Text className="text-xs font-medium text-primary">
+          Carbon Emission
+        </Text>
         <Text className="text-sm font-bold text-primary">
           {value ? `${value} kg` : "0 kg"}
         </Text>
